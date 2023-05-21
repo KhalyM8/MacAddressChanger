a6419533f64b26395e0836896c69b5d02fd6c316
KhalyM8 <alynas789@gmail.com>
2023-05-21 12:51:49 +0100

    MacChanger finished program

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def get_arguments():
     parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
     parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 
-    (options, arguments) = parser.parse_args()
+    (arguments) = parser.parse_args()
 
     if not options.interface:
         parser.error("[-] please specify an interface, use --help for more info")
